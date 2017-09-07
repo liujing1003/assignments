06d1f44a39c7b8071e023eb6baee1854936379d3
liujing1003 <31439456+liujing1003@users.noreply.github.com>
2017-09-07 16:56:02 +0800

    Add files via upload

---
+++ 数组课后题/最长单词.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    char sentence[500] = {0}, word[50] = {0};
+    cin.getline(sentence, 500);
+    int j = 0, sizeWord = 0;
+    while (i < 500 && sentence[i] != '.')
+    {
+        if (sentence[i] == ' ')
+        {
+            if (j > sizeWord)
+            {
+                sizeWord = j;
+                for (int k = 0; k < j; k++)
+                {
+                    word[k] = sentence[i-j+k];
+                }
+            }
+            j = 0;
+        }
+        else
+        {
+            j++;
+        }
+        i++;
+    }
+    if (sentence[i] == '.')
+    {
+        if (j > sizeWord)
+        {
+            for (int k = 0; k < j; k++)
+            {
+                word[k] = sentence[i-j+k];
+            }
+        }
+    }
+    cout << word << endl;
+    return 0;
+}
---
+++ 数组课后题/矩阵交换行.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int a[5][5]={0};
+    int m=0,n=0;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+        cin>>a[i][j];
+        }
+    }
+    cin>>n>>m;
+    if(n>=0&&n<=4&&m>=0&&m<=4)
+    {
+        int b[5]={0};
+        for(int i=0;i<5;i++)
+        {
+           b[i]=a[n][i];
+           a[n][i]=a[m][i];
+           a[m][i]=b[i];
+        }
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+            {
+                cout<<setw(4)<<a[i][j];
+            }
+            cout<<endl;
+    }
+    }
+    else
+    {
+        cout<<"error"<<endl;
+    }
+    return 0;
+}
