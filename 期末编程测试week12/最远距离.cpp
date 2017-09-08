ac7336e512fa3df6481d01c5222d8fc39ca501b5
liujing1003 <31439456+liujing1003@users.noreply.github.com>
2017-09-08 13:45:38 +0800

    vector类，setprecision的使用方法

---
+++ 期末编程测试week12/最远距离.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <iomanip>
+
+#include <vector>
+
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n=0;
+    cin>>n;
+    vector<float> x(n,0),y(n,0);//声明了两个动态float数组x,y
+    float dis=0,disbiggest=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>x[i]>>y[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            dis=sqrt ((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]));
+                     if(dis>disbiggest)
+                     {
+                         disbiggest=dis;
+                     }
+        }
+    }
+    cout<<fixed<<setprecision(4)<<disbiggest<<endl;
+    return 0;
+
+}
