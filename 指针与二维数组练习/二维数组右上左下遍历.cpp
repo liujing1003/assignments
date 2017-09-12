0b75865d79be9e781ac324faae06f6e406d6480e
liujing1003 <31439456+liujing1003@users.noreply.github.com>
2017-09-12 14:14:56 +0800

    Update 二维数组右上左下遍历.cpp

--- 指针与二维数组练习/二维数组右上左下遍历.cpp
+++ 指针与二维数组练习/二维数组右上左下遍历.cpp
@@ -6,7 +6,7 @@ int main(){
     for(i=0;i<n;i++)
         for(j=0;j<m;j++)
             cin>>a[i][j];
-    for(i=0;i<=n+m-2;i++){
+    for(i=0;i<=n+m-2;i++){//总共n+m-1条对角线
         for(j=i;j>=0;j--){
             if(j<m&&i-j<n){
                 cout<<a[i-j][j]<<endl;
