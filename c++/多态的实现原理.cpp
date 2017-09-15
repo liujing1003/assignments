a53ff66d058ebcf7fedd57171c8000a6da6fde5b
liujing1003 <31439456+liujing1003@users.noreply.github.com>
2017-09-15 16:46:21 +0800

    Add files via upload

---
+++ c++/多态的实现原理.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+  int i;
+  virtual void func(){}
+  virtual void func2(){}
+};
+class B:public A
+{
+  int j;
+  void fun(){}
+};
+int main()
+{
+  cout<<sizeof(A)<<","<<sizeof(B);
+  return 0;
+}
