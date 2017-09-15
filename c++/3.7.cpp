c8fcf33b3f0e32c87b65c577401d88262dc1228c
liujing1003 <31439456+liujing1003@users.noreply.github.com>
2017-09-15 14:45:10 +0800

    Add files via upload

---
+++ c++/3.7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class Sample
+{
+public:
+  int v;
+  Sample(){};
+  Sample(int n):v(n){};
+  Sample(Sample & x)
+  {
+    v=2+x.v;
+  }
+};
+Sample PrintAndDouble(Sample o)
+{
+  cout<<o.v;
+  o.v=2*o.v;
+  return o;
+}
+int main()
+{
+  Sample a(5);
+  Sample b=a;
+  const Sample & c=PrintAndDouble(b);
+  cout<<endl;
+  cout<<c.v<<endl;
+  Sample d;
+  d=a;
+  cout<<d.v;
+}
